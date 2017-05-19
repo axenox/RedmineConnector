7a8dee57fc6aff1963f961aee2eec71490ef50e8
Andrej Kabachnik <andrej_ka@gmx.de>
2017-05-19 08:59:04 +0200

    NEW action CreateIssue

---
+++ Actions/CreateIssue.php
@@ -0,0 +1,20 @@
+<?php
+namespace axenox\RedmineConnector\Actions;
+
+use exface\Core\Actions\CreateData;
+use exface\Core\Exceptions\Actions\ActionInputInvalidObjectError;
+
+class CreateIssue extends CreateData {
+	
+	protected function perform(){
+		if (!$this->get_input_data_sheet()->get_meta_object()->is('axenox.RedmineConnector.ISSUE')){
+			throw new ActionInputInvalidObjectError($this, $this->get_alias_with_namespace() . ' can only be called on axenox.RedmineConnector.ISSUE objects!');
+		}
+		parent::perform();
+		$this->set_undoable(false);
+		$new_ticket_id = $this->get_result_data_sheet()->get_uid_column()->get_cell_value(0);
+		$this->set_result_message('New ticket <a target="_blank" href="' . $this->get_input_data_sheet()->get_meta_object()->get_data_connection()->get_url() . "issues/" . $new_ticket_id . '">#' . $new_ticket_id . '</a> created!');
+		
+	}
+	
+}
