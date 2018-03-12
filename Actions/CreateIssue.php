ee24ea1fde631ebd3ea8a16110708271cfa9bebf
Andrej Kabachnik <andrej_ka@gmx.de>
2018-03-12 16:31:00 +0100

    REFACTOR renamed TaskResult -> Result

--- Actions/CreateIssue.php
+++ Actions/CreateIssue.php
@@ -5,7 +5,7 @@ use exface\Core\Actions\CreateData;
 use exface\Core\Exceptions\Actions\ActionInputInvalidObjectError;
 use exface\Core\Interfaces\Tasks\TaskInterface;
 use exface\Core\Interfaces\DataSources\DataTransactionInterface;
-use exface\Core\Interfaces\Tasks\TaskResultInterface;
+use exface\Core\Interfaces\Tasks\ResultInterface;
 
 /**
  * Creates a new issue showing the issue id with a link to it in the result message.
@@ -16,7 +16,7 @@ use exface\Core\Interfaces\Tasks\TaskResultInterface;
 class CreateIssue extends CreateData
 {
 
-    protected function perform(TaskInterface $task, DataTransactionInterface $transaction) : TaskResultInterface
+    protected function perform(TaskInterface $task, DataTransactionInterface $transaction) : ResultInterface
     {
         $input = $this->getInputDataSheet($task);
         
@@ -26,7 +26,7 @@ class CreateIssue extends CreateData
         $result = parent::perform($task, $transaction);
         $result->setUndoable(false);
         
-        $new_ticket_id = $this->getResultDataSheet()->getUidColumn()->getCellValue(0);
+        $new_ticket_id = $result->getData()->getUidColumn()->getCellValue(0);
         $result->setMessage($this->translate('RESULT', array(
             '%url%' => $input->getMetaObject()->getDataConnection()->getUrl(),
             '%issue_id%' => $new_ticket_id
